68d56bed755fff9b31461fca3eba32de11d6778e
al2718x <al2718x@gmail.com>
2024-11-04 17:10:46 +0300

    refactor

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
 import subprocess
-# import signal
 import gi.repository
+
 gi.require_version('Gtk', '3.0')
 gi.require_version('AppIndicator3', '0.1')
 from gi.repository import GLib
@@ -11,8 +11,6 @@ from gi.repository import AppIndicator3
 
 
 class MyIndicator:
-    # processes = []
-
     def __init__(self):
         # noinspection PyArgumentList
         self.ind = AppIndicator3.Indicator.new(
@@ -22,21 +20,18 @@ class MyIndicator:
         )
         self.ind.set_status(AppIndicator3.IndicatorStatus.ACTIVE)
         self.ind.set_attention_icon('new-messages-red')
-        # self.ind.set_label('email', 'email')
         self.menu = Gtk.Menu()
-
-        # item = Gtk.MenuItem()
-        # item.set_label('Electron Mail')
-        # item.connect('activate', self.run, './ElectronMail.sh')
-        # self.menu.append(item)
-
         self.menu_item('Thunderbird', 'thunderbird', self.run, 'thunderbird')
-        self.menu_item_f('Tuta Mail', '/home/al/.local/share/icons/hicolor/16x16/apps/tutanota-desktop.png', self.run, '/home/al/tutanota/tutanota-desktop-linux.AppImage')
+        self.menu_item_f(
+            'Tuta Mail',
+            '/home/al/.local/share/icons/hicolor/16x16/apps/tutanota-desktop.png',
+            self.run,
+            '/home/al/tutanota/tutanota-desktop-linux.AppImage'
+        )
         self.menu_item('Compose', 'stock_mail-compose', self.run, ['thunderbird', '-compose'])
         self.menu_item('Address Book', 'stock_addressbook', self.run, ['thunderbird', '-addressbook'])
         self.menu_item('Calendar', 'org.gnome.Calendar', self.run, 'gnome-calendar')
         self.menu_item('Quit', 'application-exit', self.quit)
-
         self.menu.show_all()
         self.ind.set_menu(self.menu)
 
@@ -66,17 +61,10 @@ class MyIndicator:
 
     def run(self, widget, param):
         self.ind.set_status(AppIndicator3.IndicatorStatus.ATTENTION)
-        # subprocess.run(['thunderbird'])
         subprocess.Popen(param)
-        # p = subprocess.Popen(param)
-        # self.processes.append(p)
-        # print('pid=', p.pid)
         GLib.timeout_add_seconds(1, self.ind.set_status, AppIndicator3.IndicatorStatus.ACTIVE)
 
     def quit(self, widget):
-        # for pid in self.processes:
-        #     if isinstance(pid, subprocess.Popen):
-        #         pid.send_signal(sig=signal.SIGTERM)
         Gtk.main_quit()
 
     def main(self):
