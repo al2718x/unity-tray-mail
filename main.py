fc07f732b7cc703628c283b87bc8b0392825294f
al2718x <al2718x@gmail.com>
2024-11-20 00:22:35 +0300

    use ini file to add extra items

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-
+import configparser
+import os
 import subprocess
 import gi.repository
 
@@ -11,38 +12,51 @@ from gi.repository import AppIndicator3
 
 
 class MyIndicator:
+    _ini = os.path.dirname(__file__) + '/menu.ini'
     def __init__(self):
         # noinspection PyArgumentList
         self.ind = AppIndicator3.Indicator.new(
-            'Thunderbird Indicator 1.0.2',
+            'Thunderbird Indicator 1.0.3',
             'indicator-messages',
             AppIndicator3.IndicatorCategory.SYSTEM_SERVICES
         )
         self.ind.set_status(AppIndicator3.IndicatorStatus.ACTIVE)
         self.ind.set_attention_icon('new-messages-red')
         self.menu = Gtk.Menu()
-        self.menu_item('Thunderbird', icon_name='thunderbird', args='thunderbird')
-        self.menu_item(
-            'Tuta Mail',
-            icon_file='/home/al/.local/share/icons/hicolor/16x16/apps/tutanota-desktop.png',
-            args=['/home/al/tutanota/tutanota-desktop-linux.AppImage']
-        )
-        self.menu_item(
-            'Electron Mail',
-            icon_name='com.github.vladimiry.ElectronMail',
-            args=['flatpak', 'run', 'com.github.vladimiry.ElectronMail']
-        )
-        self.menu_item(
-            'Delta Chat',
-            icon_name='chat.delta.desktop',
-            args=['flatpak', 'run', 'chat.delta.desktop']
-        )
+        self.menu_item('Thunderbird', icon_name='thunderbird', args=['thunderbird'])
+        for section in self.ini_sections(self._ini):
+            self.menu_item(
+                self.ini_read(self._ini, section, 'label'),
+                icon_name=self.ini_read(self._ini, section, 'icon_name'),
+                icon_file = self.ini_read(self._ini, section, 'icon_file'),
+                args=self.ini_read(self._ini, section, 'args').split(' ')
+            )
         self.menu_item('Compose', icon_name='stock_mail-compose', args=['thunderbird', '-compose'])
         self.menu_item('Address Book', icon_name='stock_addressbook', args=['thunderbird', '-addressbook'])
         self.menu_item('Calendar', icon_name='org.gnome.Calendar', args=['gnome-calendar'])
         self.menu_item('Quit', icon_name='application-exit', action=self.quit)
         self.menu.show_all()
         self.ind.set_menu(self.menu)
+
+    @staticmethod
+    def ini_sections(file):
+        try:
+            config = configparser.ConfigParser()
+            config.read(file)
+            return config.sections()
+        except:
+            pass
+        return []
+
+    @staticmethod
+    def ini_read(file, section, option, default=None):
+        try:
+            config = configparser.ConfigParser()
+            config.read(file)
+            return config[section][option]
+        except:
+            pass
+        return default
 
     def menu_item(self, label, *, icon_name=None, icon_file=None, action=None, args=None):
         item = Gtk.ImageMenuItem()
@@ -67,10 +81,12 @@ class MyIndicator:
         subprocess.Popen(param)
         GLib.timeout_add_seconds(1, self.ind.set_status, AppIndicator3.IndicatorStatus.ACTIVE)
 
-    def quit(self, widget):
+    @staticmethod
+    def quit(widget):
         Gtk.main_quit()
 
-    def main(self):
+    @staticmethod
+    def main():
         Gtk.main()
 
 
